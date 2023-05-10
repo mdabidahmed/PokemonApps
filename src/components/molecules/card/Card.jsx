1ae469c2d5500af3adc1d5b76ebd8e8b8a0bb2bd
mdaahmed <mdaahmed@publicisgroupe.net>
2023-05-10 11:10:40 +0530

    testing setup

--- src/components/molecules/card/Card.jsx
+++ src/components/molecules/card/Card.jsx
@@ -30,7 +30,7 @@ const Card = ({pokemon}) => {
           <LinearGradient colors={ColorGenerate()}>
             <Image
               source={{
-                uri: `${process.env.IMAGE_URL}/${id}.png`,
+                uri: `https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/${item.name}.png`,
               }}
               accessibilityLabel="Pokemon Card List"
               style={CardStyles.imageCard}
